acf1b64750008c5387a001d824a718892f79fd57
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2020-02-29 16:18:47 +0100

    Create initial query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,6 +3,19 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
+    field :calculate_price, Float, null: false do
+      argument :type, String, required: true
+      argument :margin, Float, required: true
+      argument :exchange_rate, Float, required: true
+    end
+
+    def calculate_price(type:,margin:,exchange_rate:)
+      puts type
+      puts margin
+      puts exchange_rate
+      200.19
+    end
+
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
